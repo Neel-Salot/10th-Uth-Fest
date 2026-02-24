0a6b7db6a68c845df842be7aa66597c287e32611
Yugam Kothari <yugamkothari886@gmail.com>
2026-02-24 12:44:31 +0530

    Demo 2 for video

--- portal/src/pages/Home.tsx
+++ portal/src/pages/Home.tsx
@@ -23,8 +23,8 @@ import ras7 from '../assets/images/Ras/7.jpg.jpeg';
 import ras8 from '../assets/images/Ras/8.jpg.jpeg';
 import ras9 from '../assets/images/Ras/9.jpg.jpeg';
 import ras10 from '../assets/images/Ras/10.jpg.jpeg';
-import video8th from '../assets/Videos/8th.MP4';
-import video9th from '../assets/Videos/9th.MP4';
+import video8th from '../assets/Videos/8th.mp4';
+import video9th from '../assets/Videos/9th.mp4';
 // const video8th = '';
 
 
